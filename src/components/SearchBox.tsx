d01a0e9618d959ce7501e2cda8eda8030894ea16
Luqni Maulana <luqni@skyshi.com>
2023-11-30 16:06:24 +0700

    fix: add default value for search box in search page

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,7 +1,11 @@
 import { memo } from "react"
 import { Search } from "react-feather"
 
-const SearchBox: React.FC = () => {
+type Props = {
+  defaultValue?: string
+}
+
+const SearchBox: React.FC<Props> = ({ defaultValue }) => {
   return (
     <form 
       action="/search"
@@ -12,6 +16,7 @@ const SearchBox: React.FC = () => {
         type="text"
         name="query"
         required
+        defaultValue={defaultValue || ''}
         className="w-full rounded-full px-5 py-2 bg-transparent border-white border-2 text-white"
         placeholder="Search movie..."
       />
